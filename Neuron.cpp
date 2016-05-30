6f9be824a7dd60d8d283dd7756f0bec169880d5d
Urs Evora <yummybanana@live.com>
2016-05-30 13:51:21 -0400

    First implementation of Neuron

---
+++ Neuron.cpp
@@ -0,0 +1,70 @@
+/*
+Date: 5/29/2016
+Programer: Urs Evora
+Implementation file for Neurons.
+*/
+
+#include <vector>
+#include <stdlib.h> 	// random
+#include <math.h> 		// exp
+#include <time.h>			// time
+#include "Neuron.h"
+
+Neuron::Neuron(const int numSynapse, const double& learningRate)
+	:learningRate(learningRate),bias(random()), delta(NULL), lastOutput(NULL){
+	// Initialize synapse and last inputs
+	srand(time(NULL));
+	for(int i = 0; i < numSynapse; i++){
+		synapse.push_back(random());
+		lastInputs.push_back(NULL);
+	}
+}
+
+double Neuron::activate(const std::vector<double>& inputs){
+	// Check if inputs are correct size	
+	if( inputs.size() != synapse.size() ){ return -1; }	
+	lastInputs = inputs;
+	double sum = bias;
+	for( int i = 0; i < inputs.size(); i++ ){
+		sum += inputs[i] * synapse[i];
+	}
+	lastOutput = sigmoid(sum);
+	return lastOutput;
+}
+
+double Neuron::backProp(const std::vector<double>& deltaIn, 
+const std::vector<double>& forwardSynapse){
+	// Check if deltas equal forwardSynapse
+	if( deltaIn.size() != synapse.size() ){ return -1; }
+	// Calculate delta (node error)	
+	double error = 0;
+	for( int i = 0; i < deltaIn.size(); i++ ){
+		error += deltaIn[i] * forwardSynapse[i];
+	}
+	delta = error * lastOutput * (1 - lastOutput);
+	// Update synapse weights
+	for( int i = 0; i < synapse.size(); i++ ){
+		synapse[i] = synapse[i] - learningRate * delta * lastInputs[i];
+	}
+	return delta;
+}
+
+double Neuron::getSynapse(const int& index) const{
+	// Check if valid index
+	if( index >= synapse.size() ){ return -1;}
+	return synapse[index];
+}
+
+double Neuron::getDelta() const{
+	return delta;
+}
+
+double random() const{
+	return (double)rand() / RAND_MAX;
+}
+
+double sigmoid(const double& x) const{
+	return 1.0/(1 + exp(-x));
+}
+
+
--- Neuron.h
+++ Neuron.h
@@ -14,21 +14,34 @@ public:
 	// Contructor for Neuron. numSynapse: number of inputs synapse required for 
 	// this neuron. Typically this is the number of neurons in the previous layer,
 	// or number of features.
-	Neuron(const int numSynapse);
+	Neuron(const int& numSynapse, const double& learningRate );
 	
 	//Activates neuron and synapse. inputs: vector of input features.
 	double activate(const std::vector<double>& inputs);
 
 	//Back propagate
-	double backProp(const std::vector<double>& deltaIn); 
+	double backProp(const std::vector<double>& deltaIn, const std::vector<double>& forwardSynapse); 
 	
+	// Returns the weight of requested synapse
+	double getSynapse(const int& index);
 private:
 	std::vector<double> synapse;
 	double bias;
-	// stores last activation. Used for back propagation and debugging.
+	const double learningRate;
+	// stores last activation's inputs. Used for back propagation and debugging.
 	std::vector<double> lastInputs;
+	// stores	last activation's output. Also used for back propagation and debugging.
+	double lastOutput;
 	// error from last back propagation.	
 	double delta;
+
+	// Helper function which updates weights of the synapse during back propagate.
+	//void updateSynapse();
 }
 
+
+//Returns random double between 0 and 1.
+double random() const;
+
+double sigmoid(const double& x);
 #endif
